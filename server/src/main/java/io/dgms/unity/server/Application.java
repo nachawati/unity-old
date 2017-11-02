c3c882d5b2248ff00bba7d823caf9889ef7f72e6
Mohamad Omar Nachawati <mnachawa@gmu.edu>
2017-11-02 14:17:11 -0400

    Updated file(s)

---
+++ server/src/main/java/io/dgms/unity/server/Application.java
@@ -0,0 +1,35 @@
+/*******************************************************************************
+ * بسم الله الرحمن الرحيم
+ *
+ * In the name of Allah, the Most Compassionate, the Most Merciful
+ *
+ * This software is authored by Mohamad Omar Nachawati, 1436-1439 AH
+ *******************************************************************************/
+
+package io.dgms.unity.server;
+
+import org.glassfish.jersey.logging.LoggingFeature;
+import org.glassfish.jersey.server.ResourceConfig;
+import org.glassfish.jersey.server.ServerProperties;
+import org.glassfish.jersey.server.TracingConfig;
+import org.glassfish.jersey.server.mvc.jsp.JspMvcFeature;
+
+import io.dgms.unity.server.api.ServerDGSystem;
+
+/**
+ * @author Mohamad Omar Nachawati
+ *
+ */
+public class Application extends ResourceConfig
+{
+    /**
+     *
+     */
+    public Application()
+    {
+        packages(ServerDGSystem.class.getPackage().getName());
+        this.register(JspMvcFeature.class);
+        register(LoggingFeature.class);
+        property(ServerProperties.TRACING, TracingConfig.ON_DEMAND.name());
+    }
+}
